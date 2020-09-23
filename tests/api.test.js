2793d2da53d478549ebcd29d7e57c6f1dff728f1
Etienne Dldc <e.deladonchamps@gmail.com>
2020-09-23 16:57:40 +0200

    Use test()

--- tests/api.test.js
+++ tests/api.test.js
@@ -17,15 +17,15 @@ describe('API', () => {
     await fse.remove(path.resolve(process.cwd(), process.env.SQLITE_FILE_PATH));
   });
 
-  it('should respond on home', async () => {
+  test('should respond on home', async () => {
     await supertest(server).get('/').expect(200, { you: '<- are here !' });
   });
 
-  it('should get empty movies', async () => {
+  test('should get empty movies', async () => {
     await supertest(server).get('/movies').expect(200, []);
   });
 
-  it('should signup', async () => {
+  test('should signup', async () => {
     const res = await supertest(server)
       .post('/users/signup')
       .send({
@@ -47,7 +47,7 @@ describe('API', () => {
     ]);
   });
 
-  it('should return the token when login', async () => {
+  test('should return the token when login', async () => {
     await db.knex.table('users').insert([
       {
         user_id: '2d43c86c-76a9-4566-a107-09d25d56fad3',
@@ -65,7 +65,7 @@ describe('API', () => {
     expect(res.body.token).toBe('NDMU1Dx97LxOpCOe7wE86');
   });
 
-  it('should return 403 if wrong password', async () => {
+  test('should return 403 if wrong password', async () => {
     await db.knex.table('users').insert([
       {
         user_id: '2d43c86c-76a9-4566-a107-09d25d56fad3',
